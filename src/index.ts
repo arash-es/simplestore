84d36da1a7305180a191dc0a201c30dac3b3489e
arashray <arash.es.1378@gmail.com>
2022-08-18 20:30:05 +0430

    feat: use sync external store

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,23 @@
 import { Dispatch, SetStateAction } from "react";
-import { useSyncExternalStore } from "use-sync-external-store/shim";
+import { useSyncExternalStoreWithSelector } from "use-sync-external-store/shim/with-selector";
 
-interface Notifier<T> {
-  (): void;
-  getState: (data: T) => unknown;
-}
+type Notifier = () => void;
 
 function createStore<T = any>(data: T) {
-  const listeners = new Set<Notifier<T>>();
-
+  const listeners = new Set<Notifier>();
   const dispatch: Dispatch<SetStateAction<T>> = (setStateAction) => {
-    const newData: T = typeof setStateAction === "function" ? (setStateAction as Function)(data) : setStateAction;
-    const prevData = data;
-    data = newData;
-
-    listeners.forEach((notifier) => {
-      if (notifier.getState(prevData) !== notifier.getState(newData)) {
-        notifier();
-      }
-    });
+    data = typeof setStateAction === "function" ? (setStateAction as Function)(data) : setStateAction;
+    listeners.forEach((notifier) => notifier());
   };
-
-  const useStore = <G = T>(getState: (state: T) => G) => {
-    const subscribe = (notify: () => void) => {
-      const notifier: Notifier<T> = Object.assign(notify, { getState });
+  // @ts-ignore
+  const useStore = <G = T>(getState: (state: T) => G = (d) => d) => {
+    const subscribe = (notifier: Notifier) => {
       listeners.add(notifier);
       return () => listeners.delete(notifier);
     };
-    const getSnapShot = () => getState(data);
-
-    return useSyncExternalStore(subscribe, getSnapShot);
+    const getSnapShot = () => data;
+    return useSyncExternalStoreWithSelector(subscribe, getSnapShot, undefined, getState);
   };
-
   return { dispatch, useStore };
 }
 
